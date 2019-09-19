a00b32f70b67d92e1099f7f6791aae56239b1fee
Simas <simonas.deimantavicius@mediapark.com>
2019-09-19 13:47:43 +0300

    add mongoose

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const express = require('express');
 const app = express();
 
 /**
+ * @middlewares
+ */
+
+/**
  * @routes
  */
 app.get('/', (req, res) => {
   res.send('Ahoy Sailor o/');
+})
+app.get('/posts', (req, res) => {
+  res.send('Posts Route !');
 })
 
  /**
